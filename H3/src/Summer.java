1e7bfe348dfd32563ff8d3b1614c08b856f0500d
Ananda Badmaev <st055663@student.spbu.ru>
2024-03-27 07:43:18 +0300

    fix H3 reducer

--- H3/src/Summer.java
+++ H3/src/Summer.java
@@ -1,22 +1,25 @@
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.*;
-import org.apache.hadoop.mapreduce.lib.output.*;
-import org.apache.hadoop.util.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Summer
-extends Reducer<Text, IntWritable, Text, IntWritable>
-{
-    public void reduce(Text key, Iterable<IntWritable> values, Context context)
-	throws IOException, InterruptedException
-	{
-        int sum = 0;
-        for (IntWritable val : values) {
-            sum += val.get();
+public class Summer extends Reducer<Text, Text, Text, Text> {
+    public void reduce(Text key, Iterable<Text> values, Context context)
+    throws IOException, InterruptedException
+    {
+        Map<Text, Integer> freqs = new HashMap<>();
+        for (Text value : values) {
+            freqs.put(value, freqs.getOrDefault(value, 0) + 1);
         }
-        context.write(key, new IntWritable(sum));
+        Text mostFreq = freqs.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(new Text("@isolated word@"));
+
+        context.write(key, mostFreq);
     }
 }
